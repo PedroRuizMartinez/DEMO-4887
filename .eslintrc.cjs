fa3bb97bbca810a30ceff81e3f4786644844c083
Pedro A. Ruiz <pedro.ruiz.dv@gmail.com>
2024-02-01 10:05:44 +0100

    Configuración prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': [
+      'warn',
+    ],
+    '@typescript-eslint/no-unused-vars': [
+        'warn'
+    ]
   },
 }
